91b4a61c8697bd82da4319bb87f24989b5fa281f
jrarandaarcos <jrarandaarcos@gmail.com>
2025-05-10 13:19:09 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<HeaderProps> = ({ onOpenTutorial, onOpenStats }) => {
     <header className="bg-black text-white p-4 flex items-center justify-between">
       <div className="flex items-center">
         <img 
-          src="https://image.tmdb.org/t/p/original/4IcVSKtK0e5RIIpFnD29xU0P5mI.png" 
+          src="https://gimgs2.nohat.cc/thumb/f/350/image-file-formats-haikyuu-png-picture-png-download-630-450--comdlpng6951853.jpg" 
           alt="Volleyball" 
           className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 mr-3"
         />
