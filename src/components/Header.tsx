6c4d3c4f5b563d2ca9f8c8ce2057764541ef2c7e
jrarandaarcos <jrarandaarcos@gmail.com>
2025-05-10 14:12:47 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC<HeaderProps> = ({ onOpenTutorial, onOpenStats }) => {
     <img 
           src="https://logos-world.net/wp-content/uploads/2021/08/Haikyuu-Logo.png" 
           alt="Volleyball" 
-          className="w-full h-auto flex item-center space-x-4"></img>
+          className="w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64 h-auto flex item-center space-x-4"></img>
       </div>
 
 <h1 className="text-2xl md:text-3xl font-bold text-center flex-grow text-orange-500">¡Guess the Hakyuu character!</h1>      
